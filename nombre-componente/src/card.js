e19843aeda8e61e2cd9997d9d340dfae54621cd7
Cristopher <alancristophercr@gmail.com>
2022-09-01 22:04:09 -0500

    termino de clase 1

---
+++ nombre-componente/src/card.js
@@ -0,0 +1,10 @@
+const Hello = () => {
+    return (
+        <div className="card" >
+            <h2>Sedan</h2>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+    )
+};
+
+export default Hello
